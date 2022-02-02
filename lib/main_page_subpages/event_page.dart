5f47836f837610e076051de73694ef2de0ac4945
Soun Sean Kim <sukim2406@gmail.com>
2022-02-02 23:13:21 +0900

    SOSODAY Project : adding Calendar ::::: Daily Commit Project 02-02-2022

--- lib/main_page_subpages/event_page.dart
+++ lib/main_page_subpages/event_page.dart
@@ -1,15 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
-class EventPage extends StatelessWidget {
+class EventPage extends StatefulWidget {
   const EventPage({Key? key}) : super(key: key);
+
+  @override
+  _EventPageState createState() => _EventPageState();
+}
+
+class _EventPageState extends State<EventPage> {
+  var _selectedDay;
+  var _focusedDay;
+  var _calendarFormat = CalendarFormat.month;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text(
-        'Event',
-        style: TextStyle(fontSize: 60),
-      ),
-    );
+        child: Column(
+      children: [
+        SizedBox(height: 200),
+        TableCalendar(
+          firstDay: DateTime.utc(2020, 01, 01),
+          lastDay: DateTime.utc(2030, 12, 31),
+          focusedDay: DateTime.now(),
+          selectedDayPredicate: (day) {
+            return isSameDay(_selectedDay, day);
+          },
+          onDaySelected: (selectedDay, focusedDay) {
+            setState(() {
+              _selectedDay = selectedDay;
+              _focusedDay = focusedDay;
+            });
+          },
+          calendarFormat: _calendarFormat,
+          onFormatChanged: (format) {
+            setState(() {
+              _calendarFormat = format;
+            });
+          },
+          onPageChanged: (focusedDay) {
+            _focusedDay = focusedDay;
+          },
+        ),
+      ],
+    ));
   }
 }
